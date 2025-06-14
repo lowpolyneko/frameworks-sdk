59a0b70eed7a78752784a661a5933be03eb5da28
Khalid Hossain <khossain4337@gmail.com>
2025-06-14 00:35:14 +0000

    Added missing flag for torch_ccl source build, still failing but more predictably.

--- pytorch_2p7/102.3-torch_ccl_2p7_form_source.sh
+++ pytorch_2p7/102.3-torch_ccl_2p7_form_source.sh
@@ -14,7 +14,11 @@ unset CMAKE_ROOT
 #export PATH="/opt/aurora/24.347.0/spack/unified/0.9.2/install/linux-sles15-x86_64/gcc-13.3.0/gcc-13.3.0-4enwbrb/bin":$PATH
 
 TMP_WORK=/lus/flare/projects/Aurora_deployment/datascience/software/envs/wheel_factory/torch_ccl_2p7
-conda activate /lus/flare/projects/Aurora_deployment/datascience/software/envs/conda_envs/pt2p7_ipex2p7
+# It seems torch_ccl requires an IPEX installation for it to build the XPU .so
+# files. The environment below has a pip installed version of IPEX-2.7.0
+# from the publicly available wheel, which was probably built with 
+# oneapi/2025.0.x
+conda activate /lus/flare/projects/datasets/softwares/envs/env_pip_pytorch_2p7
 
 echo "Activated the conda environment in $(which conda)"
 
@@ -45,6 +49,7 @@ export CPATH=$CONDA_PREFIX/include:$CPATH
 #export CMAKE_PREFIX_PATH="${CONDA_PREFIX:-'$(dirname $(which conda))/../'}:${CMAKE_PREFIX_PATH}"
 #export CMAKE_PREFIX_PATH="/opt/aurora/24.347.0/spack/unified/0.9.2/install/linux-sles15-x86_64/gcc-13.3.0/gcc-13.3.0-4enwbrb/."
 export DPCPP_ROOT=$(realpath $(dirname $(which icpx))/..)
+export COMPUTE_BACKEND=dpcpp
 export _GLIBCXX_USE_CXX11_ABI=1 
 
 export CXX=$(which g++)
@@ -67,6 +72,6 @@ python setup.py clean
 MAX_JOBS=48 python setup.py bdist_wheel --verbose --dist-dir ${TMP_WORK}
 
 echo ""
-echo "Completed build IPEX wheel"
+echo "Completed build oneccl_bindings_for_pytorch wheel"
 echo ""
 
