93698a89951f6ee59bbdf6c25fe2832eec627ca1
Khalid Hossain <khossain4337@gmail.com>
2025-09-04 14:33:54 +0000

    Different release candidate environments

---
+++ pytorch_2p8_release_build_scripts/sunspot/env_frameworks_RC4.sh
@@ -0,0 +1,265 @@
+#!/bin/bash -x
+# This environment with RC4 tag tries to pin mlflow==3.3.1, it might fix an issue
+# with CosmicTagger-latest to run. Needs to check if torchtune behaves normally
+# Time Stamp
+tstamp() {
+     date +"%Y-%m-%d-%H%M%S"
+}
+## Proxies to clone from a compute node
+export HTTP_PROXY=http://proxy.alcf.anl.gov:3128
+export HTTPS_PROXY=http://proxy.alcf.anl.gov:3128
+export http_proxy=http://proxy.alcf.anl.gov:3128
+#
+CONDA_ENV_INSTALL_DIR=/lus/tegu/projects/datasets/software/wheelforge/envs/conda_envs
+CONDA_ENV_NAME=frameworks_2025.2.0_RC4
+
+source /opt/aurora/25.190.0/spack/unified/0.10.0/install/linux-sles15-x86_64/gcc-13.3.0/miniforge3-24.3.0-0-gfganax/bin/activate
+
+ENVPREFIX=$CONDA_ENV_INSTALL_DIR/$CONDA_ENV_NAME
+CONDA_ENV_MANIFEST=${CONDA_ENV_INSTALL_DIR}/manifests/${CONDA_ENV_NAME}
+
+rm -rf ${CONDA_ENV_MANIFEST}
+mkdir -p ${CONDA_ENV_MANIFEST}
+ 
+rm -rf ${ENVPREFIX}
+mkdir -p ${ENVPREFIX}
+
+export CONDA_PKGS_DIRS=${ENVPREFIX}/../.conda/pkgs
+export PIP_CACHE_DIR=${ENVPREFIX}/../.pip
+
+echo "Creating Conda environment with Python 3.10.14"
+conda create python=3.10.14 --prefix ${ENVPREFIX} --override-channels \
+           --channel https://software.repos.intel.com/python/conda/linux-64 \
+           --channel conda-forge \
+           --strict-channel-priority \
+           --yes
+
+conda activate ${ENVPREFIX}
+
+echo "Conda is coming from $(which conda)"
+
+# Load default modules with oneapi/2025.2.0 with PTI 0.12.3 on Sunspot
+# Load hdf5 module, this loads hdf5/1.14.6
+module restore
+module load cmake
+unset CMAKE_ROOT
+module load pti-gpu
+module load hdf5
+
+TMP_WORK=/lus/tegu/projects/datasets/software/wheelforge/envs/conda_envs
+cd $TMP_WORK
+
+LOCAL_WHEEL_LOC=/lus/tegu/projects/datasets/software/wheelhouse
+#TRY_WHEEL_LOC=${LOCAL_WHEEL_LOC}/try_wheels
+
+pip install --no-cache-dir -r ${LOCAL_WHEEL_LOC}/torchdata_0.11.0_torchao_0.12.0_h5py_3.14.0_torchvision_0.23.0_ipex_2.8.10_pytorch_2.8.0_triton_xpu_3.4.0_combined_requirements.txt
+pip install --no-cache-dir -r ${LOCAL_WHEEL_LOC}/legacy_nre_requirements.txt
+
+set +e
+
+rm_conda_pkgs=(
+        "dpcpp-cpp-rt"
+        "impi_rt"
+        "intel-cmplr-lib-rt"
+        "intel-cmplr-lib-ur"
+        "intel-cmplr-lic-rt"
+        "intel-gpu-ocl-icd-system"
+        "intel-opencl-rt"
+        "intel-openmp"
+        "intelpython"
+        "intel-sycl-rt"
+        "level-zero"
+        "libedit"
+        "mkl"
+        "mkl_fft"
+        "mkl_random"
+        "mkl-service"
+        "mkl_umath"
+        "onemkl-sycl-blas"
+        "onemkl-sycl-dft"
+        "onemkl-sycl-lapack"
+        "onemkl-sycl-rng"
+        "onemkl-sycl-stats"
+        "onemkl-sycl-vm"
+        "pyedit"
+        "tbb"
+        "tcm"
+        "umf"
+        "tcmlib"
+        "intel-pti"
+        "impi-rt"
+        "oneccl"
+        "oneccl-devel"
+        "onemkl-sycl-sparse"
+    )
+
+for pkg in "${rm_conda_pkgs[@]}"
+do
+    conda uninstall $pkg \
+        --prefix ${ENVPREFIX} \
+        --force \
+        --yes
+    pip uninstall $pkg -y
+done
+
+pip uninstall -y numpy numpy-base
+pip install --no-cache-dir numpy==2.0.2
+#pip install --no-cache-dir pytorch-triton-xpu==3.4.0 --index-url https://download.pytorch.org/whl/nightly/
+pip install --no-cache-dir ${LOCAL_WHEEL_LOC}/pytorch_triton_xpu-*.whl
+
+for pkg in "${rm_conda_pkgs[@]}"
+do
+    conda uninstall $pkg \
+        --prefix ${ENVPREFIX} \
+        --force \
+        --yes
+    pip uninstall $pkg -y
+done
+
+pip install --no-deps --no-cache-dir --force-reinstall $LOCAL_WHEEL_LOC/torch-*.whl
+pip install --no-deps --no-cache-dir --force-reinstall $LOCAL_WHEEL_LOC/intel_extension_for_pytorch-*.whl
+pip install --no-deps --no-cache-dir --force-reinstall $LOCAL_WHEEL_LOC/torchvision-*.whl
+pip install --no-deps --no-cache-dir --force-reinstall $LOCAL_WHEEL_LOC/mpi4py-*.whl
+pip install --no-deps --no-cache-dir --force-reinstall $LOCAL_WHEEL_LOC/h5py-*.whl
+pip install --no-deps --no-cache-dir --force-reinstall $LOCAL_WHEEL_LOC/torchdata-*.whl
+
+
+mkdir -p ${CONDA_ENV_NAME}
+
+export CXX=$(which g++)
+export CC=$(which gcc)
+echo "CXX = $CXX"
+echo "CC = $CC"
+
+export REL_WITH_DEB_INFO=1
+export USE_CUDA=0
+export USE_XPU=1
+export USE_XCCL=1
+
+pip uninstall -y mkl mkl-include
+pip install --no-cache-dir -r ${LOCAL_WHEEL_LOC}/torchao_0.12.0_xpu_separate_requirements.txt
+pip install --no-deps --no-cache-dir --force-reinstall $LOCAL_WHEEL_LOC/torchao-*.whl
+
+for pkg in "${rm_conda_pkgs[@]}"
+do
+    conda uninstall $pkg \
+        --prefix ${ENVPREFIX} \
+        --force \
+        --yes
+    pip uninstall $pkg -y
+done
+
+pip install --no-cache-dir -r ${LOCAL_WHEEL_LOC}/torchtune_0.6.1_xpu_separate_requirements.txt
+
+for pkg in "${rm_conda_pkgs[@]}"
+do
+    conda uninstall $pkg \
+        --prefix ${ENVPREFIX} \
+        --force \
+        --yes
+    pip uninstall $pkg -y
+done
+
+pip install --no-deps --no-cache-dir --force-reinstall $LOCAL_WHEEL_LOC/torchtune-*.whl
+
+pip install outlines_core==0.2.10 --no-deps
+pip install --no-cache-dir -r ${LOCAL_WHEEL_LOC}/vllm_0.10.1_xpu_separate_requirements.txt
+
+for pkg in "${rm_conda_pkgs[@]}"
+do
+    conda uninstall $pkg \
+        --prefix ${ENVPREFIX} \
+        --force \
+        --yes
+    pip uninstall $pkg -y
+done
+
+## vllm's complex inter-dependency sometimes brings own triton, triton-xpu
+pip uninstall -y triton pytorch-triton pytorch-triton-xpu
+
+## installing back our preferred version
+#pip install --no-cache-dir pytorch-triton-xpu==3.4.0 --index-url https://download.pytorch.org/whl/nightly/
+pip install --no-cache-dir ${LOCAL_WHEEL_LOC}/pytorch_triton_xpu-*.whl
+
+for pkg in "${rm_conda_pkgs[@]}"
+do
+    conda uninstall $pkg \
+        --prefix ${ENVPREFIX} \
+        --force \
+        --yes
+    pip uninstall $pkg -y
+done
+
+pip install --no-deps --no-cache-dir --force-reinstall $LOCAL_WHEEL_LOC/vllm-*.whl
+
+pip install --no-cache-dir -r ${LOCAL_WHEEL_LOC}/deepspeed_0.17.5_xpu_specific_requirements.txt
+
+for pkg in "${rm_conda_pkgs[@]}"
+do
+    conda uninstall $pkg \
+        --prefix ${ENVPREFIX} \
+        --force \
+        --yes
+    pip uninstall $pkg -y
+done
+
+pip uninstall nvidia-nccl-cu12 -y
+
+pip install --no-deps --no-cache-dir deepspeed==0.17.5
+echo ""
+echo "Finished installing the deepspeed/0.17.5 and dependencies"
+echo ""
+
+for pkg in "${rm_conda_pkgs[@]}"
+do
+    conda uninstall $pkg \
+        --prefix ${ENVPREFIX} \
+        --force \
+        --yes
+    pip uninstall $pkg -y
+done
+
+pip install --no-deps --no-cache-dir --force-reinstall $LOCAL_WHEEL_LOC/scikit_learn_intelex-*.whl
+
+pip install --no-cache-dir -r ${LOCAL_WHEEL_LOC}/dpctl_0.20.0_xpu_requirements.txt
+
+for pkg in "${rm_conda_pkgs[@]}"
+do
+    conda uninstall $pkg \
+        --prefix ${ENVPREFIX} \
+        --force \
+        --yes
+    pip uninstall $pkg -y
+done
+
+pip install --no-deps --no-cache-dir --force-reinstall $LOCAL_WHEEL_LOC/dpctl-*.whl
+pip install --no-deps --no-cache-dir --force-reinstall $LOCAL_WHEEL_LOC/dpnp-*.whl
+## This is a wheel built from Intel-dpcpp-llvm-spirv archived package, dependecy of numba-dpex/0.23.0
+## This package does not have any dependency other than Python >= 3.10
+## https://github.com/IntelPython/dpcpp-llvm-spirv/tree/main
+## Modified to match Aurora compute image. Courtesy: Christopher Chan-Nui
+## We will source, maintain and version control this modified package
+pip install --no-deps --no-cache-dir --force-reinstall $LOCAL_WHEEL_LOC/aurora_dpcpp_llvm_spirv-*.whl
+pip install --no-deps --no-cache-dir --force-reinstall $LOCAL_WHEEL_LOC/numba_dpex-*.whl
+
+echo ""
+echo "Finished installing the environment: frameworks/2025.2.0_RC4"
+echo ""
+
+
+## Final clean up
+for pkg in "${rm_conda_pkgs[@]}"
+do
+    conda uninstall $pkg \
+        --prefix ${ENVPREFIX} \
+        --force \
+        --yes
+    pip uninstall $pkg -y
+done
+
+echo ""
+echo "Writing the package lists"
+conda list > $CONDA_ENV_MANIFEST/${CONDA_ENV_NAME}_conda_env.list 2>&1
+pip list > $CONDA_ENV_MANIFEST/${CONDA_ENV_NAME}_pip.list 2>&1
+echo "Package list writing finished"
+
