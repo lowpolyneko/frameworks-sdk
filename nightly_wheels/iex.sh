c98ac1334e84f7693808b17cb7d77c7c10595f4e
Ethan Wong <ewong@anl.gov>
2026-01-12 14:48:00 -0600

    ipex: use `ci-lib.sh`

--- nightly_wheels/iex.sh
+++ nightly_wheels/iex.sh
@@ -1,14 +1,42 @@
 #!/bin/sh
 
+set -xe
+source ../ci-lib.sh
+
+# 0) Get pytorch wheel as argument
+while getopts 't:' opt; do
+    case "$opt" in
+        t)
+            TORCH_WHEEL="$(realpath $OPTARG)";;
+    esac
+done
+
+if [ -z "$TORCH_WHEEL" ]; then
+    echo "Usage: $0 -t <pytorch_wheel>" 1>&2
+    exit 1
+fi
+
+# 1) Pull source and gen build environment
+gen_build_dir_with_git 'https://github.com/intel/intel-extension-for-pytorch' -b xpu-main
+setup_build_env
+pushd 'intel-extension-for-pytorch'
+
+uv venv --python 3.12
+uv pip install --no-cache --link-mode=copy -r requirements.txt pip "$TORCH_WHEEL"
+
+# 2) Set IPEX build configuration
+export CC="$(which gcc)"
+export CXX="$(which g++)"
+export INTELONEAPIROOT="$ONEAPI_ROOT"
+export MAX_JOBS=16
+
 # 3) Build Intel Extension for PyTorch
-git clone https://github.com/intel/intel-extension-for-pytorch
-cd intel-extension-for-pytorch
-git checkout xpu-main
-git submodule sync && git submodule update --init --recursive
-#pip install -r requirements.txt
-MAX_JOBS=16 INTELONEAPIROOT="$ONEAPI_ROOT" \
-    CC="$(which gcc)" CXX="$(which g++)" \
-    python setup.py bdist_wheel \
-    > "ipex-build-whl-$(date +%Y-%m-%d-%H%M%S).log" 2>&1
-pip install dist/*.whl
-cd ../
+# build_bdist_wheel 'intel-extension-for-pytorch'
+source .venv/bin/activate
+python setup.py bdist_wheel
+deactivate
+popd
+
+# 4) Cleanup
+archive_artifacts 'intel-extension-for-pytorch'
+cleanup_build_dir
