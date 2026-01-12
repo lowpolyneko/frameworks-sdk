4993a896aaa8c3172b85fefaca7d3d246e38b581
Ethan Wong <ewong@anl.gov>
2026-01-12 15:23:04 -0600

    oneccl: use `ci-lib.sh`

--- nightly_wheels/oneccl.sh
+++ nightly_wheels/oneccl.sh
@@ -1,15 +1,24 @@
 #!/bin/sh
 
-# 4) Build oneCCL bindings
-git clone https://github.com/intel/torch-ccl
-cd torch-ccl
-#FIXME need to checkout c27ded5 or create a patch because the repo version.txt is messed up
-git checkout c27ded5
-git submodule sync && git submodule update --init --recursive
-#pip install -r requirements.txt
-ONECCL_BINDINGS_FOR_PYTORCH_BACKEND=xpu \
-    INTELONEAPIROOT="$ONEAPI_ROOT" USE_SYSTEM_ONECCL=ON COMPUTE_BACKEND=dpcpp \
-    python setup.py bdist_wheel \
-    > "ccl-build-whl-$(date +%Y-%m-%d-%H%M%S).log" 2>&1
-pip install dist/*.whl
-cd ../
+set -xe
+source ../ci-lib.sh
+
+# 1) Pull source and gen build environment
+gen_build_dir_with_git 'https://github.com/intel/torch-ccl'
+setup_build_env
+
+# FIXME need to checkout c27ded5 or create a patch because the repo version.txt is messed up
+# git checkout c27ded5
+
+# 2) Set torch-ccl build configuration
+export ONECCL_BINDINGS_FOR_PYTORCH_BACKEND='xpu'
+export INTELONEAPIROOT="$ONEAPI_ROOT"
+export USE_SYSTEM_ONECCL='ON'
+export COMPUTE_BACKEND='dpcpp'
+
+# 3) Build
+build_bdist_wheel 'torch-ccl'
+
+# 4) Cleanup
+archive_artifacts 'torch-ccl'
+cleanup_build_dir
